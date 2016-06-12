37af671f2cd17b08e8f76b168afb7f1ade1c3ce0
dy <wdyxzkq@163.com>
2016-06-12 14:19:44 +0800

    add bootstrap_time.php

---
+++ bootstrap_time.php
@@ -0,0 +1,10 @@
+<div class="form-group">
+    <label class="col-sm-2 control-label">播种日期</label>
+    <div class="col-md-4">
+        <input type="text" name="seed_seeddate" onclick="laydate({istime: true, format: 'YYYY-MM-DD'})" class="form-control layer-date"  placeholder="请输入播种日期"><label class="laydate-icon"></label>
+        <span class="help-block m-b-none"></span>
+    </div>
+</div>
+
+<!--laydate-->
+<script src="__PUBLIC__/Admin/js/plugins/layer/laydate/laydate.js"></script>
