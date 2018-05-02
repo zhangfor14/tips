56755d036767b57b0c2a84c12d45eaf7d107c3cf
Dy <wdyxzkq@163.com>
2018-05-02 13:55:50 +0800

    linux_定时运行_crontab.php

--- linux_定时运行_crontab.php
+++ linux_定时运行_crontab.php
@@ -94,3 +94,138 @@ crontab -l
 */1 * * * * sleep 6 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
 ......
 签到人数，每3秒增加2人
+
+
+//以下为示例
+#同步时钟
+*/30 * * * * ntpdate 210.72.145.44
+
+MAILTO=""
+
+#清理业务垃圾日志
+#仅仅保留3天
+0 3 * * * find /data/web/web/Application/Runtime/Logs/* -type f -mtime +3 -exec rm -rf {} \;
+
+
+
+#stop ruthout service
+#0 3 * * * /data/batstop 
+#start ruthout service
+#1 3 * * * /data/batstart
+#backup mysql data
+1 2 * * * /data/mysqlbackup.sh
+
+#cut nginx log
+0 0 * * * /bin/bash /opt/rh/cut_log.sh
+
+#parse nginx access log
+#0 1 * * * /opt/remi/php54/root/usr/bin/php /data/RuthServer/crontab/parse_www_log.php
+#0 1 * * * /opt/remi/php54/root/usr/bin/php /data/RuthServer/crontab/parse_api_log.php
+
+#statistic
+0 1 * * * /opt/remi/php54/root/usr/bin/php /data/RuthServer/crontab/stats.php
+
+#恢复php-fpm
+15 1 * * * /sbin/service php-fpm restart
+
+#清理svn 三天前备份
+0 3 * * * find /data/svn_bk/* -type d -mtime +3 -exec rm -rf {} \;
+
+#一分钟去拉取一次用户的提交信息
+*/1 * * * * sleep 10 && /usr/bin/python /root/code_db/download_from_wx.py >>/root/code_db/meeting_id.log
+*/1 * * * * sleep 20 && /usr/bin/python /root/code_db/download_from_wx.py >>/root/code_db/meeting_id.log
+*/1 * * * * sleep 30 && /usr/bin/python /root/code_db/download_from_wx.py >>/root/code_db/meeting_id.log
+*/1 * * * * sleep 40 && /usr/bin/python /root/code_db/download_from_wx.py >>/root/code_db/meeting_id.log
+*/1 * * * * sleep 50 && usr/bin/python /root/code_db/download_from_wx.py >>/root/code_db/meeting_id.log
+
+#定时执行付费问答，1提示专家24小时未回复 2提示用户专家36小时内回复了 3提示用户由于专家超36小时未回复，退款
+*/1  * * * *  /usr/bin/curl "http://server.ruthout.com/?mod=qa&act=remindAsk&do=remindAskUser"
+*/2  * * * *  /usr/bin/curl "http://server.ruthout.com/?mod=qa&act=remindAsk"
+
+#对微课视频进行10%截取，用于微课分享
+*/1 * * * * /usr/bin/python /root/soft/ffmpeg_process.py
+
+#用于1分钟查询一次未解决的问题
+*/2 * * * * python /root/soft/cmd_exec_worker/post_ask_reminder.py
+
+#防止gearman/openoffice server挂掉，如果挂掉就重启
+*/1 * * * * /bin/sh /root/soft/script/gearman_check.sh
+
+#清理cached的缓存，每1个小时清洗一次
+#0 */1 * * * sync;echo 3 > /proc/sys/vm/drop_caches
+
+#文档转码,pdf和swf，每2分钟一次
+*/2 * * * * python /root/soft/cmd_exec_worker/doc_2pdf_2swf_oo.py
+
+#用户注册同步，从ruth到fmalldb
+*/5 * * * * python /root/soft/cmd_exec_worker/db_sync_2fmall.py
+
+#扫描nginx 日志，查询攻击的地址，然后nginx封闭IP一天
+*/5 * * * * /bin/sh /root/soft/cmd_exec_worker/nginx_monitor/ip_blocks.sh
+*/2 * * * * /bin/sh /root/soft/cmd_exec_worker/nginx_monitor/ip_blocks_server.sh
+#*/5 * * * * /bin/sh /root/soft/cmd_exec_worker/nginx_monitor/ip_blocks_wap.sh
+
+#解封被封掉的IP
+0 1 * * * /bin/sh /root/soft/cmd_exec_worker/nginx_monitor/clear_ips.sh
+
+#签到人数 0点-8点，11点-17点，每3秒增加2人
+#每3秒调用一次
+#* * * * * /bin/sh /usr/local/crontab/crontab_sign_data.sh
+#* * * * * sleep 3;curl http://www.ruthout.com/index.php/ApiUser/addSignData
+#* * * * * sleep 3; /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * *  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 3 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 6 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 9 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 12 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 15 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 18 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 21 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 24 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 27 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 30 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 33 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 36 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 39 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 42 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 45 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 48 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 51 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 54 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+*/1 * * * * sleep 57 &&  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/addSignData"
+#每晚11点执行 一次清理昨天签到假数据
+#00 23 * * * /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/deleSinFakeData"
+#每20秒执行一下定时发帖检查，如果有定时发贴，则进行发贴
+*/1 * * * * sleep 20 &&  /usr/bin/curl "http://admin.priv.ruthout.com/admin.php/Crontab/doAddCrontabCircleTopic"
+*/1 * * * * sleep 40 &&  /usr/bin/curl "http://admin.priv.ruthout.com/admin.php/Crontab/doAddCrontabCircleTopic"
+
+#每天早上7点执行一次投票数量随机加5-10次 (2017-09-22日前)
+#00 07 * * * /usr/bin/curl "http://www.ruthout.com/TempVoteDraw/addRandVote"
+
+#最赞hr活动，每一分钟执行一次给审核通过的专家发送短信通知
+#*/1  * * * *  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/sendExamieHrPassMsg"
+
+#峰会报名活动，每一分钟执行一次给审核通过的用户发送短信通知
+#*/1  * * * *  /usr/bin/curl "http://www.ruthout.com/index.php/ApiCrontab/sendExamieMeetingEnroll"
+
+#峰会报名活动，每一分钟执行一次给审核用户发微信消息通知
+#*/1  * * * *  /usr/bin/curl "http://www.ruthout.com/Enrollmeeting2/pushEnrollStateWxMsg"
+
+#峰会邀约报名，每一分钟执行一次被邀约人报名成功，更新分享表报名状态，并发给邀约者收益微信通知
+#*/1  * * * *  /usr/bin/curl "http://www.ruthout.com/Enrollmeeting2/updateShreWx"
+
+#峰会邀约报名，每一分钟执行一次峰会提现处理,发放微信现金红包
+*/1  * * * *  /usr/bin/curl "http://www.ruthout.com/Wxhongbao/sendHongBao"
+
+#sync;echo 3 > /proc/sys/vm/drop_caches
+
+#定时执行hr_sas项目更新，如果有新版本则更新，没有则不更新
+* * * * * /bin/sh /data/soft/cmd_exec_worker/svn_up_hr_sas/crontab_up.sh
+
+#建立索引
+0 2 * * * /usr/java/jdk1.7.0_65/bin/java -cp /data/ruth_rpc/search-index-1.0-jar-with-dependencies.jar com.ruthout.index.CreateQASearchIndex >>/data/ruth_rpc/index.log
+
+#峰会每天微信对账信息
+#0 2 * * * /usr/bin/python /root/code_db/WxpayAPI_php_log.py
+
+
